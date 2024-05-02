194aee058754bce64873f187ce377b292c302a9c
fabiankent01 <fabiankent01@gmail.com>
2024-05-02 17:57:36 +0100

    new

--- scripts/getDates.js
+++
@@ -1,7 +0,0 @@
-let oLastModif = new Date(document.lastModified)
-
-document.getElementById("lastModified").textContent = "Last Modification: " + oLastModif;
-let year = new Date();
-let fullYear = year.getFullYear();
-document.getElementById("currentYear").textContent = fullYear;
-
